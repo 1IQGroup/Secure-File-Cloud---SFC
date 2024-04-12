a2dd74d32d2c4ba6a552e4a160bcfb89c305f8a6
Jakub Martyński <69093918+XPenguin2002@users.noreply.github.com>
2024-04-12 14:22:20 +0200

    php do strony jeszcze bez user i admin

---
+++ projekt/skrypty/haslo.php
@@ -0,0 +1,62 @@
+<?php
+// Sprawdzenie, czy dane zostały przesłane z formularza
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET['kod'])) {
+    // Pobranie danych z formularza
+    $haslo = $_POST['haslo'];
+    $kod = $_GET['kod'];
+
+    // Połączenie z bazą danych
+    include('baza.php');
+    $polaczenie = mysqli_connect($host, $uzytkownik_bd, $haslo_bd, $bd);
+
+    // Sprawdzenie, czy udało się połączyć z bazą danych
+    if (!$polaczenie) {
+        die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
+    }
+
+    // Pobranie emaila użytkownika na podstawie kodu z tabeli rejestracja
+    $query = "SELECT email FROM rejestracja WHERE kod='$kod'";
+    $result = mysqli_query($polaczenie, $query);
+
+    if ($result && mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        $email = $row['email'];
+
+        // Wstawienie nowego hasła i emaila użytkownika do tabeli uzytkownicy
+        // Sprawdzenie, czy adres e-mail istnieje w bazie danych
+        $checkQuery = "SELECT * FROM uzytkownicy WHERE email = '$email'";
+        $checkResult = mysqli_query($polaczenie, $checkQuery);
+
+        if (mysqli_num_rows($checkResult) > 0) {
+            // Adres e-mail istnieje - wykonaj zapytanie aktualizacji hasła
+            $updateQuery = "UPDATE uzytkownicy SET haslo = SHA2('$haslo', 256) WHERE email = '$email'";
+            $updateResult = mysqli_query($polaczenie, $updateQuery);
+        } else {
+            // Adres e-mail nie istnieje - wykonaj zapytanie dodania nowego użytkownika
+            $insertQuery = "INSERT INTO uzytkownicy (email, haslo) VALUES ('$email', SHA2('$haslo', 256))";
+            $insertResult = mysqli_query($polaczenie, $insertQuery);
+        }
+        if ($insertResult||$updateResult) {
+            // Usunięcie rekordu z tabeli rejestracja
+            $deleteQuery = "DELETE FROM rejestracja WHERE kod='$kod'";
+            $deleteResult = mysqli_query($polaczenie, $deleteQuery);
+            if ($deleteResult) {
+                header("location: ../index.php?login=1");
+            } else {
+                echo "Wystąpił błąd podczas usuwania rekordu z tabeli rejestracja.";
+            }
+        } else {
+            echo "Wystąpił błąd podczas zapisywania nowego hasła do bazy danych.";
+        }
+    } else {
+        echo "Nie znaleziono użytkownika dla podanego kodu.";
+    }
+
+    // Zamknięcie połączenia z bazą danych
+    mysqli_close($polaczenie);
+} else {
+    // Jeśli dane nie zostały przesłane, przekierowanie na stronę główną
+    header("location: ../index.php");
+    exit;
+}
+?>
---
+++ projekt/skrypty/przypomnij.php
@@ -0,0 +1,117 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Include Composer's autoloader
+require 'vendor/autoload.php';
+
+// Połączenie z bazą danych
+include('baza.php');
+
+// Połączenie z bazą danych
+$polaczenie = mysqli_connect($host, $uzytkownik_bd, $haslo_bd, $bd);
+
+// Sprawdzenie czy połączenie udało się nawiązać
+if (!$polaczenie) {
+    die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
+}
+
+// Pobranie adresu IP użytkownika
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// Sprawdzenie, czy adres IP użytkownika jest zablokowany w bazie danych
+$query = "SELECT * FROM blokada2 WHERE ip='$ip'";
+$result = mysqli_query($polaczenie, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    // Adres IP użytkownika znajduje się w tabeli blokada, przekierowanie na stronę główną
+    header("location: ../przypomnij.php?error=2");
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Pobranie adresu e-mail z formularza
+    $email = $_POST['email'];
+    
+    // Sprawdzenie, czy podany e-mail istnieje już w bazie danych
+    $query = "SELECT * FROM uzytkownicy WHERE email = ?";
+    if ($stmt = mysqli_prepare($polaczenie, $query)) {
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        $emailExists = mysqli_stmt_num_rows($stmt) > 0;
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "Błąd przygotowania zapytania SQL: " . mysqli_error($polaczenie);
+        exit;
+    }
+    mysqli_close($polaczenie);
+    if (!$emailExists) {
+        header("Location: ../przypomnij.php?error=1");
+        exit;
+    }
+
+    // Tworzenie nowej instancji PHPMailera
+    $mail = new PHPMailer(true); // true oznacza, że zostanie wyrzucony wyjątek w przypadku błędu
+
+    try {
+        // Konfiguracja serwera SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp-relay.brevo.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'j.martynski2002@gmail.com'; // Twój adres e-mail
+        $mail->Password = 'XgH207kqGEKS8Cah'; // Twoje hasło do konta
+        $mail->SMTPSecure = 'auto';
+        $mail->Port = 587;
+
+        // Konfiguracja nadawcy i odbiorcy
+        $mail->setFrom('j.martynski2002@gmail.com', 'noreply@1iqteam.pl'); // Twój adres e-mail jako nadawca
+        $mail->addAddress($email); // Adres e-mail odbiorcy
+
+        // Treść wiadomości
+        $mail->isHTML(true); // Ustawienie formatu wiadomości na HTML
+        
+        // Wygenerowanie losowego kodu
+        $kod = bin2hex(random_bytes(10)); // Długość 20 znaków w formacie szesnastkowym
+        
+        // Dodanie danych do tabeli rejestracja
+        $polaczenie = mysqli_connect($host, $uzytkownik_bd, $haslo_bd, $bd);
+        $query = "INSERT INTO rejestracja (email, kod) VALUES (?, ?)";
+    
+        // Przygotowanie i wykonanie instrukcji przygotowanej
+        if ($stmt = mysqli_prepare($polaczenie, $query)) {
+            // Związanie parametrów
+            mysqli_stmt_bind_param($stmt, "ss", $email, $kod);
+        
+            // Wykonanie zapytania
+            mysqli_stmt_execute($stmt);
+
+            // Zwolnienie wyniku zapytania
+            mysqli_stmt_close($stmt);
+        } else {
+            // Obsługa błędu przygotowania zapytania
+            echo "Błąd przygotowania zapytania SQL: " . mysqli_error($polaczenie);
+        }
+        // Zamknięcie połączenia z bazą danych (przeniesione na koniec skryptu)
+        // mysqli_close($polaczenie);
+
+        // Treść wiadomości
+        $mail->Subject = 'odzyskiwanie hasla';
+        $mail->Body = "Zmien swoje haslo tutaj:<br>
+        <a href='https://www.jakubmartynski.pl/projekt/haslo.php?kod=$kod'>https://www.jakubmartynski.pl/projekt/haslo.php?kod=$kod</a><br>
+        Link wazny przez godzine.";
+
+        // Wysłanie wiadomości
+        $mail->send();
+        echo 'Wiadomość e-mail została wysłana na adres: ' . $email;
+        $insertQuery = "INSERT INTO blokada2 (ip) VALUES ('$ip')";
+        mysqli_query($polaczenie, $insertQuery);
+        
+    } catch (Exception $e) {
+        echo 'Wystąpił błąd podczas wysyłania wiadomości: ', $mail->ErrorInfo;
+    }
+
+    // Zamknięcie połączenia z bazą danych (przeniesione na koniec skryptu)
+    mysqli_close($polaczenie);
+}
+?>
